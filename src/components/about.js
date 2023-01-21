1e3a4f78d946c69f580c5be24efc91c56e1ecf77
Prathya Thakur <prathya2410@gmail.com>
2023-01-21 22:44:10 +0530

    adding files to repo

---
+++ src/components/about.js
@@ -0,0 +1,16 @@
+import React from 'react';  //imporing for using our context (useContext)
+// import NoteContext from '../context/notes/notescontext';  //importing context, else have to provide path in usecontext
+
+const About = () => {
+  return (
+    <div>
+      <div><h2 className='mx-5 mb-3'> About Page </h2></div>
+      <div className="mx-5">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis deserunt saepe incidunt laudantium inventore mollitia enim cumque aut facere excepturi a at eos animi porro ipsum veniam, ullam iusto repudiandae laborum! Sed expedita, iusto sapiente exercitationem, harum ullam debitis incidunt quo voluptatibus ab doloremque earum vitae deleniti iure vero animi! Velit at, delectus omnis quia dolor obcaecati ut eos unde excepturi saepe aliquid cumque eius vero itaque sequi nesciunt repellat voluptatum veritatis dolorum odit sed, ex iste. Corrupti a delectus, commodi et est tenetur, iste id, deleniti placeat accusamus labore ut dicta provident cupiditate praesentium aliquid. Placeat labore a nisi quis eius adipisci ullam sed ab nulla numquam? Nesciunt sapiente sunt consequatur. Quis nostrum eos sint aliquid hic quo nemo fugit, dignissimos quas rem veniam consectetur suscipit incidunt odit quasi corporis consequatur! Perferendis explicabo aliquid molestiae esse aliquam architecto alias autem expedita in ab provident iure, doloribus ullam quibusdam assumenda accusantium, nemo totam voluptate iusto? Tempore asperiores debitis assumenda totam molestias? Voluptate corporis iusto neque dolore maxime delectus praesentium numquam eos velit hic qui aspernatur illum, minus aperiam repudiandae, recusandae assumenda, doloremque dignissimos nemo repellendus facilis fugit tenetur. Dolor labore ratione eveniet distinctio voluptatem, illo doloribus iusto! Deserunt, placeat? Natus!</p>
+      </div>
+      
+    </div>
+  )
+}
+
+export default About;
